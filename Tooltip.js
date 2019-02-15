6b17e5a5058fdb8536ac726ccf7daeeba1bbac50
mitcheesta <m.estanol99@gmail.com>
2019-02-15 12:46:01 -0500

    initial conversion to OOP-like

--- Tooltip.js
+++ Tooltip.js
@@ -4,10 +4,27 @@
 //     element.addEventListener('onmousehover', alert());
 // });
 
+
+
+class Definition{
+    constructor(term, meaning){
+        this.term = term;
+        this.meaning = meaning;
+    }
+};
+
+
+//Pseudo-database of definitions
+var definitionArr = [];
+definitionArr.push('tooltip', 'A tooltip offers additional information on a subject matter');
+
+
 //Purpose: function to display hidden info for 
 function show(ttInfo){
 /*   var arrTT = document.getElementsByClassName('tt-name__tooltip');
    for(var tt in arrTT){
         
     } */
+
+    
 }
