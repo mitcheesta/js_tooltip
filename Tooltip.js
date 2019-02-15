7ebb26652588b51bdc5502551fdd6942b132b0db
mitcheesta <m.estanol99@gmail.com>
2019-02-15 13:42:30 -0500

    basic functionality achieved

--- Tooltip.js
+++ Tooltip.js
@@ -1,30 +1,38 @@
-// var arrays = document.getElementsByClassName("tooltip");
-
-// arrays.array.forEach(element => {
-//     element.addEventListener('onmousehover', alert());
-// });
-
-
-
 class Definition{
     constructor(term, meaning){
-        this.term = term;
-        this.meaning = meaning;
+        this.term_ = term;
+        this.meaning_ = meaning;
     }
 };
 
-
 //Pseudo-database of definitions
 var definitionArr = [];
-definitionArr.push('tooltip', 'A tooltip offers additional information on a subject matter');
+definitionArr.push(new Definition('tooltip', 'A tooltip offers additional information on a subject matter.'));
+//definitionArr.push 
 
+//Purpose: function to display definition for each term
+function show(element, term){
+    let definition = definitionArr.find(definition => definition.term_ == term);
 
-//Purpose: function to display hidden info for 
-function show(ttInfo){
-/*   var arrTT = document.getElementsByClassName('tt-name__tooltip');
-   for(var tt in arrTT){
-        
+    //alert version
+   /*  
+    if(typeof(definition) == 'object'){
+        alert(definition.meaning_);
+    }
+    else{
+        alert(typeof(definition));
     } */
 
-    
+
+    let p = document.createElement('p');
+    p.id = term + "_meaning";
+    p.className = "meaning";
+    let text = document.createTextNode(definition.meaning_);
+    p.append(text);
+    element.parentElement.append(p);
+
+}
+
+function hide(element, term){
+    document.getElementById(term + "_meaning").outerHTML = "";
 }
