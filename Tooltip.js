651cd13fc4968a2f4642232968e44f9073b7c489
mitcheesta <m.estanol99@gmail.com>
2019-02-15 13:58:28 -0500

    added CSS and comments

--- Tooltip.js
+++ Tooltip.js
@@ -1,3 +1,4 @@
+//Storing terms and their meanings in Definition class
 class Definition{
     constructor(term, meaning){
         this.term_ = term;
@@ -5,34 +6,28 @@ class Definition{
     }
 };
 
-//Pseudo-database of definitions
+//Array of Definition objects functioning as pseudo-database of definitions
 var definitionArr = [];
 definitionArr.push(new Definition('tooltip', 'A tooltip offers additional information on a subject matter.'));
-//definitionArr.push 
 
-//Purpose: function to display definition for each term
+//Purpose: Finds definition for term and adds it to an element that is displayed under the term in the HTML.
+// @element - the element that called this function (passed in as 'this')
+// @term - the term passed in as a string
 function show(element, term){
     let definition = definitionArr.find(definition => definition.term_ == term);
 
-    //alert version
-   /*  
-    if(typeof(definition) == 'object'){
-        alert(definition.meaning_);
-    }
-    else{
-        alert(typeof(definition));
-    } */
-
-
+    //Creates <p> with id and class
     let p = document.createElement('p');
     p.id = term + "_meaning";
-    p.className = "meaning";
+    p.className = "tt-meaning";
+
+    //Adds term meaning as text to <p> then adds that to parent of calling element
     let text = document.createTextNode(definition.meaning_);
     p.append(text);
     element.parentElement.append(p);
-
 }
 
-function hide(element, term){
+//Purpose: 
+function hide(term){
     document.getElementById(term + "_meaning").outerHTML = "";
 }
