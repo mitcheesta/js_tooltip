3dc563bc1bf7f810509975774e0baaeffadfc64d
mitcheesta <m.estanol99@gmail.com>
2019-02-15 12:23:51 -0500

    initial commit

---
+++ Tooltip.js
@@ -0,0 +1,13 @@
+// var arrays = document.getElementsByClassName("tooltip");
+
+// arrays.array.forEach(element => {
+//     element.addEventListener('onmousehover', alert());
+// });
+
+//Purpose: function to display hidden info for 
+function show(ttInfo){
+/*   var arrTT = document.getElementsByClassName('tt-name__tooltip');
+   for(var tt in arrTT){
+        
+    } */
+}
