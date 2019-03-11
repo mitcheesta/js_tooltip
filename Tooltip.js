e38de072deed30478774284806e1ed54b06dc006
mitcheesta <m.estanol99@gmail.com>
2019-03-11 11:42:18 -0400

    removed pseudo-database and allow user to pass in definition in HTML

--- Tooltip.js
+++ Tooltip.js
@@ -6,24 +6,17 @@ class Definition{
     }
 };
 
-//Array of Definition objects functioning as pseudo-database of definitions
-var definitionArr = [];
-definitionArr.push(new Definition('tooltip', 'A tooltip offers additional information on a subject matter.'));
-definitionArr.push(new Definition('lorem ipsum', 'A placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content.'));
-
 //Purpose: Finds definition for term and adds it to an element that is displayed under the term in the HTML.
 // @element - the element that called this function (passed in as 'this')
 // @term - the term passed in as a string
-function show(element, term){
-    let definition = definitionArr.find(definition => definition.term_ == term);
-
+function show(element, term, definition){
     //Creates <span> with id and class
     let e = document.createElement('span');
     e.id = term + "_meaning";
     e.className = "tt-meaning";
 
     //Adds term meanings as inner text
-    let text = document.createTextNode(definition.meaning_);
+    let text = document.createTextNode(definition);
     e.append(text);
     element.append(e);
 
